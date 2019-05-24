be1af3e53f8048f2e9e9963178a3d772c7967ac4
Connor Finholt <cfinholt@dwolla.com>
2019-05-24 17:25:51 -0500

    Firebase Integration

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+const config = {
+  apiKey: "AIzaSyB-jlvthXsKjxhn97qrJ6ol3wIEh3Q1UGk",
+  authDomain: "wine-on-the-rocks.firebaseapp.com",
+  databaseURL: "https://wine-on-the-rocks.firebaseio.com",
+  projectId: "wine-on-the-rocks",
+  storageBucket: "wine-on-the-rocks.appspot.com",
+  messagingSenderId: "992431972045",
+  appId: "1:992431972045:web:057be46fb6d0f7da"
+};
+firebase.initializeApp(config);
+export default firebase;
